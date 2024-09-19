388b61c2cd1c646d7b71cef6fe244a4f35adf871
Sergey Bulavskiy <sergey.bulavskiy@mcc-soft.de>
2024-09-19 14:58:47 +0700

    Inject dependency via code behind.

---
+++ Frontend/Frontend.App/Components/Pages/Patients.razor.cs
@@ -0,0 +1,17 @@
+﻿using Frontend.Http;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc.Razor.Internal;
+
+namespace Frontend.App.Components.Pages;
+
+public partial class Patients()
+{
+    [Inject]
+    private PatientsServiceClient _patientsServiceClient { get; set; }
+    private List<PatientDto>? _patients;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _patients = await _patientsServiceClient.GetPatients();
+    }
+}
