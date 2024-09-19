73a65792321a797343c325f6c26439380ab1fadf
Sergey Bulavskiy <sergey.bulavskiy@mcc-soft.de>
2024-09-19 20:16:15 +0700

    Add additional libs and adjust scripts to be able to reuse common code.

--- MedicalDataService/build-and-push-image.ps1
+++ MedicalDataService/build-and-push-image.ps1
@@ -1,6 +1,6 @@
 $ContainerName = "docker.io/sergeybulavskiy/medical-data-service:latest"
 $ImageName = "medical-data-service"
 
-docker build -t $ImageName -f Dockerfile .
+docker build -t $ImageName -f Dockerfile ../
 docker tag $ImageName $ContainerName
 docker push $ContainerName
--- PatientsService/build-and-push-image.ps1
+++ PatientsService/build-and-push-image.ps1
@@ -1,6 +1,6 @@
 $ContainerName = "docker.io/sergeybulavskiy/patients-service:latest"
 $ImageName = "patients-service"
 
-docker build -t $ImageName -f Dockerfile .
+docker build -t $ImageName -f Dockerfile ../
 docker tag $ImageName $ContainerName
 docker push $ContainerName
