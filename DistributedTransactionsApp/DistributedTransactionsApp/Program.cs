4fe5d0504cb1825c6d6822e56784251347e088bd
Sergey Bulavskiy <sergey.bulavskiy@mcc-soft.de>
2024-11-19 19:07:26 +0700

    Add WaitFor for resources.

--- DistributedTransactionsApp/DistributedTransactionsApp/Program.cs
+++ DistributedTransactionsApp/DistributedTransactionsApp/Program.cs
@@ -15,17 +15,22 @@ var medicalDb = postgres.AddDatabase("medicalDb");
 var patientsDataDb = postgres.AddDatabase("patientsDb");
 
 builder.AddProject<MedicalDataService_App>("medical-data")
+    .WaitFor(messaging)
+    .WaitFor(medicalDb)
     .WithReference(messaging, "RabbitMQConnection")
     .WithReference(medicalDb, "DefaultConnection");
 
 var patientsDataService = builder.AddProject<PatientsService_App>("patients-data")
     .WithExternalHttpEndpoints()
+    .WaitFor(messaging)
+    .WaitFor(patientsDataDb)
     .WithReference(patientsDataDb, "DefaultConnection")
     .WithReference(messaging, "RabbitMQConnection");
 
 builder
     .AddProject<Frontend_App>("frontend")
     .WithExternalHttpEndpoints()
+    .WaitFor(patientsDataService)
     .WithReference(patientsDataService);
 
 
